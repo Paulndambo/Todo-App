a202daf2210bb029fb238e838f29719ed82b7e96
paul ndambo ngila <paulkadabo@gmail.com>
2020-11-04 18:11:17 +0300

    First commit

---
+++ main/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.utils import timezone
+from . models import Todo
+from django.http import HttpResponseRedirect
+
+def index(request):
+    todo_items = Todo.objects.all().order_by("-added_date")
+    return render(request, 'index.html', {
+    "todo_items": todo_items
+    })
+
+def add_todo(request):
+    current_date = timezone.now()
+    content = request.POST["content"]
+    created_obj = Todo.objects.create(added_date=current_date, text=content)
+    print(created_obj)
+    print(created_obj.id)
+    return HttpResponseRedirect("/")
+
+def delete_todo(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    return HttpResponseRedirect("/")
